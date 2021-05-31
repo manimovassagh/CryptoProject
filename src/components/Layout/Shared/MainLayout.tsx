621dfc68949cf3bc0efe54f8d16f5080a022fce5
Mani Movassagh Ghazani <mani.mgh@gmail.com>
2021-05-31 14:09:04 +0200

    working on logo

--- src/components/Layout/Shared/MainLayout.tsx
+++ src/components/Layout/Shared/MainLayout.tsx
@@ -5,7 +5,7 @@ import { Layout, Menu } from 'antd';
 import { MenuUnfoldOutlined, MenuFoldOutlined, AreaChartOutlined, EuroCircleOutlined, } from '@ant-design/icons';
 import 'antd/dist/antd.css';
 import { Switcher } from './Switcher'
-
+import { Image } from 'antd';
 const { Header, Sider, Content } = Layout;
 
 export function MainLayout() {
@@ -16,10 +16,10 @@ export function MainLayout() {
   return (
     <BrowserRouter>
       <Layout>
-        {/* part one is a sider  */}
+        {/* part one is a sider !!header is in part 2!!!! */}
         <Sider trigger={null} collapsible collapsed={collapsed}>
           <NavLink to="/" >
-            <div className="logo" >
+            <div style={{ textAlign: 'center' }} className="logo" >
             </div>
           </NavLink>
           <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
@@ -33,7 +33,7 @@ export function MainLayout() {
             </Menu.Item>
           </Menu>
         </Sider>
-        {/* part two is a main layout  */}
+        {/* part 2 is a main layout including header  */}
         <Layout className="site-layout">
           <Header className="site-layout-background" style={{ padding: 0 }}>
             {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
