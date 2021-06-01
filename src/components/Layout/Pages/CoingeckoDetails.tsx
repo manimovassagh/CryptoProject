f488ad1d971d88dca4720dd07ccbaa5c4b28dc2a
Mani Movassagh Ghazani <mani.mgh@gmail.com>
2021-06-01 12:13:38 +0200

    guard

--- src/components/Layout/Pages/CoingeckoDetails.tsx
+++ src/components/Layout/Pages/CoingeckoDetails.tsx
@@ -3,28 +3,32 @@ import { useParams } from 'react-router-dom'
 import { useCoingeckoCryptoApi } from '../../CustomHooks/Coingecko.CryptoApi'
 import { CoingekoDetails } from '../../Types/CoingekoDetailsType'
 import LoadingSpinner from '../LoadingSpinner/LoadingSpinner'
-import { Button, Col, Divider, Image, Row, Statistic, Typography } from 'antd';
+import { Button, Card, Col, Divider, Image, Row, Statistic, Typography } from 'antd';
+import { ArrowUpOutlined } from '@ant-design/icons'
 import * as _ from 'lodash'
-import { values } from 'lodash'
 
 export function CoingeckoDetails(): ReactElement {
   const { id } = useParams<any>()
   const [coingekoCoins] = useCoingeckoCryptoApi<CoingekoDetails>("GET", `coins/${id}?market_data=false&community_data=false&developer_data=false&sparkline=false`)
-  // console.log(coingekoCoins)
 
+  // Helper Functions for Calculation
+
+  //#region 
   const handleTickerPriceFilter = (_coinEuroFilter: CoingekoDetails | undefined) => {
     return _coinEuroFilter?.tickers.filter(coin => coin.target === "EUR")
   }
+
   const calculateAveragePricePerEuro = (_coinEuroPriceList: CoingekoDetails | undefined) => {
     const _filteredList = handleTickerPriceFilter(_coinEuroPriceList)
     return _.round(_.meanBy(_filteredList, 'last'), 2)
   }
+  //#endregion
 
-  calculateAveragePricePerEuro(coingekoCoins)
   const coinEuroFilter = handleTickerPriceFilter(coingekoCoins)
-  if (!coinEuroFilter) { return <LoadingSpinner /> }
-  if (!coingekoCoins) { return <LoadingSpinner /> }
-  // console.log(coinEuroFilter)
+  if (!coinEuroFilter || !coingekoCoins) { return <LoadingSpinner /> }
+
+  console.log(coinEuroFilter)
+  console.log(coingekoCoins)
   return (
     <>
       <Row align={'middle'}>
@@ -40,13 +44,38 @@ export function CoingeckoDetails(): ReactElement {
           <Typography.Title>{coingekoCoins.name}</Typography.Title>
         </Col>
 
-
         <Col span={8}>
-          <Statistic title="Active Users" value={112893} />
+          <div className="site-statistic-demo-card">
+            <Row gutter={16}>
+              <Col span={12}>
+                <Card>
+                  <Statistic
+                    title="Active"
+                    value={11.28}
+                    precision={2}
+                    valueStyle={{ color: '#3f8600' }}
+                    prefix={<ArrowUpOutlined />}
+                    suffix="%"
+                  />
+                </Card>
+              </Col>
+              <Col span={12}>
+                <Card>
+                  <Statistic
+                    title="Idle"
+                    value={9.3}
+                    precision={2}
+                    valueStyle={{ color: '#cf1322' }}
+                    prefix={<ArrowUpOutlined />}
+                    suffix="%"
+                  />
+                </Card>
+              </Col>
+            </Row>
+          </div>
         </Col>
         <Col span={5}>
-          <Statistic title="Latest Average Price in Euro" value={`${calculateAveragePricePerEuro(coingekoCoins)} €
-`} precision={2} />
+          <Statistic title="Latest Average Price in Euro" value={`${calculateAveragePricePerEuro(coingekoCoins)} €`} precision={2} />
           <Button style={{ marginTop: 16 }} type="primary">
             Back to Home
       </Button>
