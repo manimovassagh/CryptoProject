0e5f12102ed3d6c86c4dd875cfc9944335e8cf60
Mani Movassagh Ghazani <mani.mgh@gmail.com>
2021-06-01 11:03:17 +0200

    divider<

--- src/components/Layout/Pages/CoingeckoDetails.tsx
+++ src/components/Layout/Pages/CoingeckoDetails.tsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom'
 import { useCoingeckoCryptoApi } from '../../CustomHooks/Coingecko.CryptoApi'
 import { CoingekoDetails } from '../../Types/CoingekoDetailsType'
 import LoadingSpinner from '../LoadingSpinner/LoadingSpinner'
-import { Button, Col, Image, Row, Statistic, Typography } from 'antd';
+import { Button, Col, Divider, Image, Row, Statistic, Typography } from 'antd';
 
 
 export function CoingeckoDetails(): ReactElement {
@@ -21,48 +21,49 @@ export function CoingeckoDetails(): ReactElement {
   if (!coingekoCoins) { return <LoadingSpinner /> }
   console.log(coinEuroFilter)
   return (
-    <Row align={'middle'}>
+    <>
+      <Row align={'middle'}>
 
-      <Col span={2}>
-        <Image
-          width={100}
-          src={coingekoCoins.image.large}
-        />
-      </Col>
-
-      <Col span={12}>
-        <Typography.Title>{coingekoCoins.name}</Typography.Title>
-      </Col>
-
-
-      <Col span={8}>
-        <Statistic title="Active Users" value={112893} />
-      </Col>
-      <Col span={12}>
-        <Statistic title="Account Balance (CNY)" value={112893} precision={2} />
-        <Button style={{ marginTop: 16 }} type="primary">
-          Back to Home
-      </Button>
-      </Col>
-      <Col span={12}>
-        <Statistic title="Active Users" value={112893} loading />
-      </Col>
-
-
-      <Row >
-        <Col span={12}>
-          <h1>{coinEuroFilter?.map((_coin, index) =>
-            <div key={index}>
-              <div> {_coin.market.name}</div>
-              <div> {_coin.last}</div>
-            </div>
-          )}</h1>
-        check
+        <Col span={2}>
+          <Image
+            width={100}
+            src={coingekoCoins.image.large}
+          />
         </Col>
+
+        <Col span={12}>
+          <Typography.Title>{coingekoCoins.name}</Typography.Title>
+        </Col>
+
+
+        <Col span={8}>
+          <Statistic title="Active Users" value={112893} />
+        </Col>
+        <Col span={5}>
+          <Statistic title="Latest Price in Euro" value={112893} precision={2} />
+          <Button style={{ marginTop: 16 }} type="primary">
+            Back to Home
+      </Button>
+        </Col>
+        <Col span={6}>
+          <Statistic title="Active Users" value={112893} loading />
+        </Col>
+
+      </Row>
+      <Divider>Price Per Euro In Diffrent Exchange Markets</Divider>
+      <Row >
+
+        <>{coinEuroFilter?.map((_coin, index) =>
+          <Col span={5} key={index}>
+            {_coin.market.name}
+            {_coin.last}
+          </Col >
+        )}</>
+        check
+
       </Row>
 
 
-    </Row>
-
+    </>
   )
 }
